409c69f65f5ef6944291ea230aada175ad596818
guliang123@hotmail.com <guliang123@hotmail.com>
2019-11-28 11:08:33 +0800

    1

---
+++ src/test/java/com/gul/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gul.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
